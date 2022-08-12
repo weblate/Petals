97331ba3992c590af524bc4674b0558fb2d7bd66
LeoColman <dev@leonardo.colman.com.br>
2022-08-12 17:27:06 -0300

    ♻️ Add configuration options to Date and Time formats
    This commit leverages Kotest test factory to make repetitive tests easier to understand.
    
    For more information on test factories check the official documentation:
    https://kotest.io/docs/framework/test-factories.html
    
    Co-authored-by: Luiz Neves Porto <dev@luiz.nevesporto.com.br>

--- app/src/main/kotlin/br/com/colman/petals/settings/SettingsRepository.kt
+++ app/src/main/kotlin/br/com/colman/petals/settings/SettingsRepository.kt
@@ -12,12 +12,30 @@ class SettingsRepository(
 ) {
 
   val currencyIcon = datastore.data.map { it[CurrencyIcon] ?: "$" }
+  val dateFormat = datastore.data.map { it[DateFormat] ?: "yyyy-MM-dd" }
+  val timeFormat = datastore.data.map { it[TimeFormat] ?: "HH:mm" }
+  val dateTimeFormat = datastore.data.map { it[DateTimeFormat] ?: "yyyy-MM-dd HH:mm" }
 
   fun setCurrencyIcon(value: String): Unit = runBlocking {
     datastore.edit { it[CurrencyIcon] = value }
   }
 
+  fun setDateFormat(value: String): Unit = runBlocking {
+    datastore.edit { it[DateFormat] = value }
+  }
+
+  fun setTimeFormat(value: String): Unit = runBlocking {
+    datastore.edit { it[TimeFormat] = value }
+  }
+
+  fun setDateTimeFormat(value: String): Unit = runBlocking {
+    datastore.edit { it[DateTimeFormat] = value }
+  }
+
   companion object {
     val CurrencyIcon = stringPreferencesKey("currency_icon")
+    val DateFormat = stringPreferencesKey("date_format")
+    val TimeFormat = stringPreferencesKey("time_format")
+    val DateTimeFormat = stringPreferencesKey("date_time_format")
   }
 }
--- app/src/test/kotlin/br/com/colman/petals/settings/SettingsRepositoryTest.kt
+++ app/src/test/kotlin/br/com/colman/petals/settings/SettingsRepositoryTest.kt
@@ -1,27 +1,49 @@
 package br.com.colman.petals.settings
 
 import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.Preferences
 import br.com.colman.petals.settings.SettingsRepository.Companion.CurrencyIcon
+import br.com.colman.petals.settings.SettingsRepository.Companion.DateFormat
+import br.com.colman.petals.settings.SettingsRepository.Companion.DateTimeFormat
+import br.com.colman.petals.settings.SettingsRepository.Companion.TimeFormat
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.core.spec.style.funSpec
 import io.kotest.engine.spec.tempfile
 import io.kotest.matchers.shouldBe
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.first
 
-class SettingsRepositoryTest : FunSpec({
+private fun SettingsTest(
+  settingName: String,
+  defaultValue: String,
+  datastoreKey: Preferences.Key<String>,
+  getValue: SettingsRepository.() -> Flow<String>,
+  setValue: SettingsRepository.(String) -> Unit
+) = funSpec {
+
   val datastore = PreferenceDataStoreFactory.create { tempfile(suffix = ".preferences_pb") }
   val target = SettingsRepository(datastore)
 
-  test("Defaults currency icon to $") {
-    target.currencyIcon.first() shouldBe "$"
-  }
+  context(settingName) {
+    test("Default") {
+      target.getValue().first() shouldBe defaultValue
+    }
 
-  test("Changes the currency to the specified one") {
-    target.setCurrencyIcon("R$")
-    target.currencyIcon.first() shouldBe "R$"
-  }
+    test("Changes to specified") {
+      target.setValue("XYZ")
+      target.getValue().first() shouldBe "XYZ"
+    }
 
-  test("Persists specified currency to permanent storage") {
-    target.setCurrencyIcon("R$")
-    datastore.data.first().get(CurrencyIcon) shouldBe "R$"
+    test("Persists new value to permanent storage") {
+      target.setValue("XYZ")
+      datastore.data.first().get(datastoreKey) shouldBe "XYZ"
+    }
   }
+}
+
+class SettingsRepositoryTest : FunSpec({
+  include(SettingsTest("Currency", "$", CurrencyIcon, SettingsRepository::currencyIcon, SettingsRepository::setCurrencyIcon))
+  include(SettingsTest("Date", "yyyy-MM-dd", DateFormat, SettingsRepository::dateFormat, SettingsRepository::setDateFormat))
+  include(SettingsTest("Time", "HH:mm", TimeFormat, SettingsRepository::timeFormat, SettingsRepository::setTimeFormat))
+  include(SettingsTest("DateTime", "yyyy-MM-dd HH:mm", DateTimeFormat, SettingsRepository::dateTimeFormat, SettingsRepository::setDateTimeFormat))
 })
